1b469be68670db803aa80cc5de4a8e33f428fb0a
Soumesh Kundu <iamsoumo26@gmail.com>
2025-11-19 00:33:31 +0530

    feat: addtional data for offers

--- utils/offers.js
+++ utils/offers.js
@@ -8,9 +8,24 @@ const HOST = process.env.HOST || 'http://localhost:3000';
 
 export const FirstOffer = {
   id: "2c",
-  title: "Get some extra sheets",
+  preTitle: 'WAIT! WE HAVE SOMETHING SPECIAL FOR YOU!',
+  title: [{
+    type: 'normal',
+    text: "Get some extra sheets for"
+  }, {
+    type: 'success',
+    text: "up to 75% OFF!"
+  }],
+  subTitle: [{
+    type: 'normal',
+    text: "Note: This is a one-time-only offer that you will never see again."
+  }],
   productTitle: "Luxe",
   productDescription: ["Thousands of our customers come back for more once they feel the life-changing differences in their sleep. Enjoy the silver-infused benefits of Miracle Made® Sheets throughout every bedroom in your home, or even keep an extra set on hand since you have this huge discount opportunity!"],
+  price: 112,
+  compareAtPrice: 112,
+  discount: 50,
+  optionsOrder: ['sizes', 'colors'],
   sizes: [
     { label: 'Twin', value: 'twin' },
     { label: 'Twin XL', value: 'twin_xl' },
@@ -40,13 +55,34 @@ export const FirstOffer = {
 const OFFERS = [
   {
     id: "3b",
-    title: "Extra Pillowcases",
+    preTitle: null,
+    title: [{
+      type: 'normal',
+      text: "Your order isn't complete without pillowcases!"
+    }],
+    subTitle: [{
+      type: 'normal',
+      text: "As a special Thank You for joining the Miracle family we want to offer you our premium Pillowcases for"
+    },
+    {
+      type: "success",
+      text: "up to 28% OFF!"
+    }
+    ],
     productTitle: "Luxe",
     productDescription: ['Our premium pillowcases are infused with silver to help prevent up to 99.7% of bacterial growth, keeping your bedding cleaner for longer. Say goodbye to bacteria build-up and rest easy on pillowcases that stay fresh and inviting—night after night'],
     features: [
       "3x less laundry",
       "Designed for a Cleaner Sleep",
       "Luxuriously soft sleep, every. single. night."
+    ],
+    price: 61,
+    compareAtPrice: 44,
+    discount: 27,
+    optionsOrder: ['fabric', 'sizes', 'colors'],
+    fabric: [
+      { label: 'Set of 2 (Luxe)', value: 'luxe' },
+      { label: 'Set of 2 (Extra Luxe)', value: 'extra_luxe' }
     ],
     sizes: [
       {
@@ -75,7 +111,18 @@ const OFFERS = [
   },
   {
     id: "5b",
-    title: "Get our Comforter",
+    preTitle: "Your Antimicrobial Bedroom Isn't Complete Without a Comforter!",
+    title: [{
+      type: 'normal',
+      text: "Get our newest silver infused comforter"
+    }, {
+      type: 'success',
+      text: "up to 54% OFF!"
+    }],
+    subTitle: [{
+      type: 'normal',
+      text: "We are offering our premium comforter to new customers only!"
+    }],
     productTitle: "Get our Comforter",
     productDescription: ['Designed to keep you at the perfect temperature all night long. To top it off, the silver-infused fibers prevent up to 99.7% of bacteria growth, helping you sleep in clean comfort.'],
     features: [
@@ -84,16 +131,20 @@ const OFFERS = [
       'Infused with silver that prevents up to 99.7% of bacteria growth',
       'Hypoallergenic and 100% vegan'
     ],
+    price: 132,
+    compareAtPrice: 132,
+    discount: 0,
+    optionsOrder: ['colors', 'sizes'],
     sizes: [
       {
-      label: 'Queen',
-      value: 'queen'
-      }, 
+        label: 'Queen',
+        value: 'queen'
+      },
       {
-      label: 'King/Cali King',
-      value: 'king/cali_king'
-      } 
-  ],
+        label: 'King/Cali King',
+        value: 'king/cali_king'
+      }
+    ],
     colors: [
       { label: 'Stone', value: 'stone' },
       { label: 'White', value: 'white' }
@@ -101,11 +152,26 @@ const OFFERS = [
   },
   {
     id: "6b",
-    title: "Miracle Made® Duvet Cover",
+    preTitle: "Wait! COMPLETE YOUR ULTIMATE SLEEP SET",
+    title: [{
+      type: "normal",
+      text: "Add a Miracle Made® Duvet Cover for"
+    }, {
+      type: "success",
+      text: "up to 47% OFF!"
+    }],
+    subTitle: [{
+      type: "normal",
+      text: "Note: This is a one-time-only offer that you will never see again."
+    }],
     productTitle: "Miracle Made® Duvet Cover",
     productDescription: ['Complete your collection for the ultimate sleep experience. Made with silver-infused Miracle Clean & Cool™ to keep you cool and prevent up to 99.7% of bacteria growth.'],
     features: [
     ],
+    price: 105,
+    compareAtPrice: 105,
+    discount: 47,
+    optionsOrder: ['sizes', 'colors'],
     sizes: [
       {
         label: 'Queen',
@@ -124,7 +190,18 @@ const OFFERS = [
   },
   {
     id: "8b",
-    title: "Mattress Protector",
+    preTitle: "WAIT! COMPLETE YOUR ULTIMATE SLEEP SET",
+    title: [{
+      type: "normal",
+      text: "Add a self-cleaning & self-cooling Mattress Protector for"
+    }, {
+      type: "success",
+      text: "up to 50% OFF!"
+    }],
+    subTitle: [{
+      type: "normal",
+      text: "Note: This is a one-time-only offer that you will never see again."
+    }],
     productTitle: "Mattress Protector",
     productDescription: ['Sleep cool and keep your mattress cleaner for longer with our silver-infused and leak-proof mattress protector that prevents up to 99.7% of bacteria growth.'],
     features: [
@@ -132,6 +209,10 @@ const OFFERS = [
       'Liquid-proof to protect from spills and stains',
       'Prevents up to 99.7% bacteria-growth',
     ],
+    price: 179,
+    compareAtPrice: 104,
+    discount: 41,
+    optionsOrder: ['sizes', 'colors'],
     sizes: [
       { label: 'Twin', value: 'twin' },
       { label: 'Full', value: 'full' },
@@ -145,7 +226,18 @@ const OFFERS = [
   },
   {
     id: "4b",
-    title: "Miracle Detergent",
+    preTitle: "Before you go, give Miracle Made Detergent Sheets a try!",
+    title: [{
+      type: "Success",
+      text: "Buy 1 Box, Get 1 FREE"
+    }, {
+      type: "normal",
+      text: "—Our Best Deal Yet!"
+    }],
+    subTitle: [{
+      type: "normal",
+      text: "Say goodbye to bulky detergent bottles and get twice the clean with our BOGO offer!"
+    }],
     productTitle: "Miracle Detergent",
     productDescription: ['Tired of bulky detergent bottles? With our Buy 1, Get 1 Free deal, enjoy less waste, cleaner laundry, and more shelf space—all while keeping your skin safe from harsh chemicals.'],
     features: [
@@ -155,26 +247,36 @@ const OFFERS = [
       'Less space, less waste',
       'No more messy spills',
     ],
+    optionsOrder: ['sizes', 'colors'],
     sizes: [
       {
-        label:"64 Loads",
+        label: "Buy 1 Get 1 FREE (64 Loads)",
         value: "64_loads"
       },
       {
-        label: "128 Loads",
+        label: "Buy 2 Get 2 FREE (128 Loads)",
         value: "128_loads"
       },
       {
-        label: "192 Loads",
+        label: "Buy 3 Get 3 FREE (192 Loads)",
         value: "192_loads"
       },
     ],
-    colors:[{
+    colors: [{
       label: 'Default', value: 'default'
     }]
   },
   {
     id: "9b",
+    preTitle: "Just Dropped",
+    title: [ {
+      type: "normal",
+      text: "Our New Robe Is Made to Breathe (and Feel Amazing)"
+    }],
+    subTitle: [{
+      type: "normal",
+      text: "Wrap yourself in clean comfort—lighter, fresher, better."
+    }],
     title: "Miracle Gauze Robe",
     productTitle: "Miracle Gauze Robe",
     productDescription: [`Slip into something softer. Our new breathable gauze robe is crafted from 4-ply cotton that feels light yet cozy—and only gets softer with every wash. Silver-treated to prevent up to 99.7% of bacteria growth, it stays fresher between wears so you can relax without the laundry stress. Designed with thoughtful details like Japanese sashiko-inspired stitching, easy patch pockets, and a mid-calf fit that flatters and flows. It's not just a robe—it’s your new evening ritual.`],
@@ -185,9 +287,13 @@ const OFFERS = [
       'Practical patch pockets and adjustable waist tie',
       'Easy to wash, hard to wear out—saves you time, effort, and money',
     ],
+    price: 89,
+    compareAtPrice: 119,
+    discount: 24,
+    optionsOrder: ['colors', 'sizes'],
     sizes: [
       {
-        label:"Small",
+        label: "Small",
         value: "small"
       },
       {
@@ -199,7 +305,7 @@ const OFFERS = [
         value: "large"
       }
     ],
-    colors:[
+    colors: [
       { label: 'White', value: 'white' },
       { label: 'Oatmeal', value: 'oatmeal' }
     ]
